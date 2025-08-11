f349b23a42afa34127c7488bf596e4af66372ffa
suraj pattanayak <126487364+Suraj-kumar-pattnayak@users.noreply.github.com>
2025-08-12 00:02:57 +0530

    Update Constructor.cpp

--- BASIC_CONCEPTS/Constructor.cpp
+++ BASIC_CONCEPTS/Constructor.cpp
@@ -51,3 +51,71 @@ int main() {
   cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
   return 0;
 }
+
+
+
+
+//extra 
+//one program describing implicit and explicit code
+
+#include <iostream>
+using namespace std;
+
+class Complex {
+public:
+    int real, imag;
+
+    // Default constructor
+    Complex() {
+        real = 0;
+        imag = 0;
+    }
+
+    // Parameterized constructor
+    Complex(int x, int y) {
+        real = x;
+        imag = y;
+    }
+
+    // Function to add two complex numbers
+    Complex addComplex(Complex c1, Complex c2) {
+        Complex temp;
+        temp.real = c1.real + c2.real;
+        temp.imag = c1.imag + c2.imag;
+        return temp;
+    }
+};
+
+int main() {
+    // ---------- Implicit call to constructor ----------
+    Complex c1(2, 3); // implicit: passing values directly when creating object
+    Complex c2(4, 5);
+
+    cout << "First complex number: " << c1.real << " + " << c1.imag << "i" << endl;
+    cout << "Second complex number: " << c2.real << " + " << c2.imag << "i" << endl;
+
+    Complex c3; // default constructor called
+    c3 = c3.addComplex(c1, c2); // adding using member function
+
+    cout << "Sum (Implicit call example): " << c3.real << " + " << c3.imag << "i" << endl;
+
+    // ---------- Explicit call to constructor ----------
+    Complex c4 = Complex(7, 8); // explicit: writing 'Complex(...)'
+    Complex c5 = Complex(1, 2);
+
+    cout << "\nThird complex number: " << c4.real << " + " << c4.imag << "i" << endl;
+    cout << "Fourth complex number: " << c5.real << " + " << c5.imag << "i" << endl;
+
+    Complex c6;
+    c6 = c6.addComplex(c4, c5);
+
+    cout << "Sum (Explicit call example): " << c6.real << " + " << c6.imag << "i" << endl;
+
+    return 0;
+}
+
+
+// →ways of calling the constructor
+// 1. Implicity call (calling the constructor using objects)
+// 2. Explicity call (constructor is called by the user)
+// 3. Dynamic initialization (Finst- memony is allocated to the objects using default constructor. then Parameter wise constructor is called to initialize the data members)
